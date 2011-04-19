7a9b266cd7127ebbb5d95afbc960e30a390863ab
barinek <barinek@gmail.com>
2011-04-18 21:52:53 -0600

    minor clean up and test rackup file

--- lib/mizuno/http_server.rb
+++ lib/mizuno/http_server.rb
@@ -3,46 +3,41 @@ module Mizuno
     include_class 'org.eclipse.jetty.server.Server'
     include_class 'org.eclipse.jetty.servlet.ServletContextHandler'
     include_class 'org.eclipse.jetty.servlet.ServletHolder'
-    include_class 'org.eclipse.jetty.server.nio.SelectChannelConnector'
     include_class 'org.eclipse.jetty.util.thread.QueuedThreadPool'
     include_class 'org.eclipse.jetty.servlet.DefaultServlet'
-
     include_class 'org.eclipse.jetty.jmx.MBeanContainer'
     include_class 'org.eclipse.jetty.server.Server'
-    include_class 'javax.management.MBeanServer'
+
     include_class 'java.lang.management.ManagementFactory'
+    include_class 'javax.management.MBeanServer'
 
     include_class 'com.mysql.jdbc.Driver'
 
     include_class 'org.slf4j.Logger'
     include_class 'org.slf4j.LoggerFactory'
-    include_class 'org.slf4j.impl.StaticLoggerBinder'
 
     @@logger = LoggerFactory.getLogger("HttpServer")
 
     def self.run(app, options = {})
       options = Hash[options.map { |option| [option[0].to_s.downcase.to_sym, option[1]] }]
 
+      port = options[:port].to_i
+
       thread_pool = QueuedThreadPool.new
       thread_pool.min_threads = 5
       thread_pool.max_threads = 50
 
-      connector = SelectChannelConnector.new
-      connector.setPort(options[:port].to_i)
-      connector.setHost(options[:host])
-
       rack_handler = RackHandler.new
       rack_handler.rackup(app)
 
-      @server = Server.new
+      @server = Server.new(port)
       @container = MBeanContainer.new(ManagementFactory.getPlatformMBeanServer)
       @server.container.addEventListener(@container)
       @server.set_thread_pool(thread_pool)
-      @server.addConnector(connector)
       @server.set_handler(rack_handler)
       @server.start
 
-      @@logger.info("Started jetty on #{connector.getHost}:#{connector.getPort}")
+      @@logger.info("Started jetty on port {}.", port)
 
       trap("SIGINT") { @server.stop and exit }
 
@@ -51,7 +46,6 @@ module Mizuno
 
     def self.stop
       @server.stop
-      @container.stop
 
       @@logger.info("Stopper jetty.")
     end
